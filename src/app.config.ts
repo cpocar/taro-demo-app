3ed7cb422f0cdc31008dfc57bbbae819c4e5e9df
杨川 <chuan.yang@aliyun.com>
2020-09-27 16:26:19 +0800

    +tabbar

--- src/app.config.ts
+++ src/app.config.ts
@@ -8,23 +8,23 @@ export default {
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
   },
-    // tabBar: {
-    //     list: [
-    //         {
-    //             pagePath: 'pages/a/index',
-    //             iconPath: 'assets/images/tabbar-service.png',
-    //             selectedIconPath: 'assets/images/tabbar-service-active.png',
-    //             text: '服务'
-    //         },
-    //         {
-    //             pagePath: 'pages/b/index',
-    //             iconPath: 'assets/images/tabbar-my.png',
-    //             selectedIconPath: 'assets/images/tabbar-my-active.png',
-    //             text: '我的'
-    //         }
-    //     ],
-    //     color: '#7F7F8B',
-    //     selectedColor: '#FF5307',
-    //     borderStyle: 'white'
-    // },
+    tabBar: {
+        list: [
+            {
+                pagePath: 'pages/a/index',
+                iconPath: 'assets/images/tabbar-service.png',
+                selectedIconPath: 'assets/images/tabbar-service-active.png',
+                text: '服务'
+            },
+            {
+                pagePath: 'pages/b/index',
+                iconPath: 'assets/images/tabbar-my.png',
+                selectedIconPath: 'assets/images/tabbar-my-active.png',
+                text: '我的'
+            }
+        ],
+        color: '#7F7F8B',
+        selectedColor: '#FF5307',
+        borderStyle: 'white'
+    },
 }
